bbed7cd2606c3c3690a16b0607586bb00d5a2f8c
Rick Elrod <rick@elrod.me>
2021-02-22 18:10:35 -0600

    update tests
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- test/test_backport.py
+++ test/test_backport.py
@@ -1,11 +1,14 @@
+import aiohttp
 import pytest
 from releasible.backport import *
 
 @pytest.fixture
-def finder():
+async def finder():
     import os
-    token = os.environ.get('GITHUB_TOKEN_RO')
-    return BackportFinder(token)
+    aio_session = aiohttp.ClientSession()
+    token = os.environ.get('GITHUB_TOKEN_RO', aio_session)
+    yield BackportFinder(token, aio_session)
+    await aio_session.close()
 
 def test_normalize_pr_url():
     ANSI_URL = 'https://github.com/ansible/ansible/pull/1234'
@@ -39,24 +42,27 @@ def test_normalize_pr_url():
         only_number=True) == 1176
 
 @pytest.mark.vcr(filter_headers=['authorization'])
-def test_prs_for_commit(finder):
-    prs = finder.prs_for_commit('997b2d2a1955ccb4e70f805c18dc3e227e86c678')
+@pytest.mark.asyncio
+async def test_prs_for_commit(finder):
+    prs = await finder.prs_for_commit(
+        '997b2d2a1955ccb4e70f805c18dc3e227e86c678')
     assert len(prs) == 1
     assert prs[0]['number'] == 73467
 
 @pytest.mark.vcr(filter_headers=['authorization'])
-def test_guess_original_pr(finder):
+@pytest.mark.asyncio
+async def test_guess_original_pr(finder):
     # Has (#nnnnn) in title
-    original_for_73544 = finder.guess_original_pr(
+    original_for_73544 = await finder.guess_original_pr(
         'https://github.com/ansible/ansible/pull/73544')
     assert original_for_73544[0]['number'] == 73541
 
     # Has a cherry-pick line
-    original_for_73493 = finder.guess_original_pr(
+    original_for_73493 = await finder.guess_original_pr(
         'https://github.com/ansible/ansible/pull/73493')
     assert original_for_73493[0]['number'] == 73487
 
     # Has a "Backport of" link
-    original_for_73067 = finder.guess_original_pr(
+    original_for_73067 = await finder.guess_original_pr(
         'https://github.com/ansible/ansible/pull/73067')
     assert original_for_73067[0]['number'] == 55
